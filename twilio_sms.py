d2cfcc159a170c514dec31f2ab2316628465d764
Ryan Dodds <rcdodds@gmail.com>
2021-02-25 22:25:27 -0500

    Make recipient optional parameter defaulting to project credential JSON file

--- twilio_sms.py
+++ twilio_sms.py
@@ -4,22 +4,21 @@ import json
 
 
 # Send a motivational text
-def send_text(text_body, to):
-    # Read in Twilio Account data from JSON file
+def send_text(text_body, to_number=None):
+    # Read in Twilio project credentials from JSON file
     with open('twilio_credentials.json', mode='r') as file:
         twilio_dict = json.load(file)
 
-    # Store required variables
-    account_sid = twilio_dict['account_sid']
-    auth_token = twilio_dict['auth_token']
-    from_number = twilio_dict['from_number']
+    # Start a Twilio client using the project credentials
+    client = Client(twilio_dict['account_sid'], twilio_dict['auth_token'])
 
-    # Start a Twilio client
-    client = Client(account_sid, auth_token)
+    # If recipient is not specified, use the default from th project credentials
+    if not to_number:
+        to_number = twilio_dict['to_number']
 
     # Send text
-    message = client.messages.create(
+    client.messages.create(
         body=text_body,
-        from_=from_number,
-        to=to
+        from_=twilio_dict['from_number'],
+        to=to_number
     )
